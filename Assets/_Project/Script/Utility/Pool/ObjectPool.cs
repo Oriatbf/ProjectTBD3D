ef8a05d5093395502f78c3789dd22030a1cdf8ac
Oriatbf <john31534249@gmail.com>
2026-01-13 16:15:16 +0900

    feat : PoolingSystemUpdate

--- Assets/_Project/Script/Utility/Pool/ObjectPool.cs
+++ Assets/_Project/Script/Utility/Pool/ObjectPool.cs
@@ -1,81 +1,158 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IPoolable
+namespace _Project.Pooling
 {
-    /// <summary>풀에서 가져왔을 때 호출</summary>
-    void OnSpawnFromPool();
+    /// <summary>
+    /// 제네릭 오브젝트 풀
+    /// </summary>
+    public class ObjectPool<T> where T : Component
+    {
+        private readonly Queue<T> pool = new Queue<T>();
+        private readonly HashSet<T> activeObjects = new HashSet<T>();
+        private readonly T prefab;
+        private readonly Transform parent;
+        private readonly int initialSize;
+        private readonly int maxSize;
+        private readonly bool isPoolable;
         
-    /// <summary>풀로 반환될 때 호출</summary>
-    void OnReturnToPool();
-        
-    /// <summary>오브젝트가 파괴될 때 호출</summary>
-    void OnDestroyFromPool();
-}
+        public int AvailableCount => pool.Count;
+        public int ActiveCount => activeObjects.Count;
+        public int TotalCount => AvailableCount + ActiveCount;
+        public int MaxSize => maxSize;
 
-public class ObjectPool<T> where T: Component, IPoolable
-{
-    private readonly Queue<T> pool = new Queue<T>();
-    private readonly T prefab;
-    private readonly Transform parent;
-    private readonly int initialSize;
-    private readonly int maxSize;
-    
-    public int Count => pool.Count;
-    public int MaxSize => maxSize;
-
-    public ObjectPool(T prefab, Transform parent, int initialSize, int maxSize)
-    {
-        this.prefab = prefab;
-        this.parent = parent;
-        this.initialSize = initialSize;
-        this.maxSize = maxSize;
-        
-        Initialize();
-    }
-
-    private void Initialize()
-    {
-        for(int i = 0; i < initialSize; i++)CreateNewObject();
-    }
-
-    private T CreateNewObject()
-    {
-        T obj = UnityEngine.Object.Instantiate(prefab, parent);
-        obj.gameObject.SetActive(false);
-        pool.Enqueue(obj);
-        return obj;
-    }
-
-    public T Get()
-    {
-        T obj;
-        if(pool.Count > 0)obj = pool.Dequeue();
-        else
+        public ObjectPool(T prefab, Transform parent, int initialSize = 10, int maxSize = 100)
         {
-            obj = CreateNewObject();
-            pool.Dequeue();
+            this.prefab = prefab;
+            this.parent = parent;
+            this.initialSize = initialSize;
+            this.maxSize = maxSize;
+            if (parent.TryGetComponent(out IPoolable poolable))
+                this.isPoolable = true;
+            else isPoolable = false;
+            
+            Initialize();
         }
-        obj.gameObject.SetActive(true);
-        obj.OnSpawnFromPool();
-        return obj;
-    }
 
-    public void Return(T obj)
-    {
-        if (obj == null) return;
-
-        obj.OnReturnToPool();
-        obj.gameObject.SetActive(false);
-        if (pool.Count >= maxSize)
+        private void Initialize()
         {
-            obj.OnDestroyFromPool();
-            UnityEngine.Object.Destroy(obj.gameObject);
+            for (int i = 0; i < initialSize; i++)
+            {
+                CreateNewObject();
+            }
         }
-        else
+
+        private T CreateNewObject()
         {
+            T obj = Object.Instantiate(prefab, parent);
+            obj.gameObject.SetActive(false);
             pool.Enqueue(obj);
+            return obj;
+        }
+
+        public T Get()
+        {
+            T obj;
+            
+            if (pool.Count > 0)
+            {
+                obj = pool.Dequeue();
+            }
+            else if (TotalCount < maxSize)
+            {
+                obj = CreateNewObject();
+            }
+            else
+            {
+                Debug.LogWarning($"Pool for {typeof(T).Name} reached max size ({maxSize}). Reusing oldest object.");
+                obj = CreateNewObject();
+            }
+            
+            obj.gameObject.SetActive(true);
+            activeObjects.Add(obj);
+            
+            if(obj.TryGetComponent(out IPoolable poolable))
+            {
+                poolable.OnSpawnFromPool();
+            }
+            
+            return obj;
+        }
+
+        public void Return(T obj)
+        {
+            if (obj == null)
+            {
+                Debug.LogWarning("Trying to return null object to pool");
+                return;
+            }
+
+            if (!activeObjects.Contains(obj))
+            {
+                Debug.LogWarning($"Trying to return an object that doesn't belong to this pool: {obj.name}");
+                return;
+            }
+
+            activeObjects.Remove(obj);
+
+            if (isPoolable && obj is IPoolable poolable)
+            {
+                poolable.OnReturnToPool();
+            }
+            
+            obj.gameObject.SetActive(false);
+            obj.transform.SetParent(parent);
+            
+            if (pool.Count >= maxSize)
+            {
+                if (isPoolable && obj is IPoolable destroyable)
+                {
+                    destroyable.OnDestroyFromPool();
+                }
+                Object.Destroy(obj.gameObject);
+            }
+            else
+            {
+                pool.Enqueue(obj);
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (var obj in activeObjects)
+            {
+                if (obj != null)
+                {
+                    if (isPoolable && obj is IPoolable poolable)
+                    {
+                        poolable.OnDestroyFromPool();
+                    }
+                    Object.Destroy(obj.gameObject);
+                }
+            }
+            activeObjects.Clear();
+
+            while (pool.Count > 0)
+            {
+                T obj = pool.Dequeue();
+                if (obj != null)
+                {
+                    if (isPoolable && obj is IPoolable poolable)
+                    {
+                        poolable.OnDestroyFromPool();
+                    }
+                    Object.Destroy(obj.gameObject);
+                }
+            }
+        }
+
+        public void Warmup(int count)
+        {
+            int toCreate = count - TotalCount;
+            for (int i = 0; i < toCreate && TotalCount < maxSize; i++)
+            {
+                CreateNewObject();
+            }
         }
     }
-
 }
